26bd913c5bdc538c9f92693efe8659b57b304e41
zvicraft <zvicraft@gmail.com>
2024-07-15 21:22:26 +0300

    add

---
+++ src/main/java/com/zvicraft/itemcompact/ItemCompact.java
@@ -0,0 +1,17 @@
+package com.zvicraft.itemcompact;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemCompact extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
